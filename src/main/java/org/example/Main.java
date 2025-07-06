ea2ff3a24ce1f629053ae4b8ec7ba95d9c4e80e3
mike <yang.chen.mike@gojek.com>
2025-07-06 15:11:21 +0800

    set up spring web

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * @author Mike Chen
  * @date 2025-01-14 21:52
  */
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        SpringApplication.run(Main.class, args);
     }
 }
